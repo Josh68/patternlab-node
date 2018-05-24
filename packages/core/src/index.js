f53d26a42db6a761b2e708198701fdb7459c212e
Josh Schneider <josh68@gmail.com>
2018-05-23 21:49:59 -0700

    a little cleanup, formatting, and refactoring

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -38,7 +38,7 @@ updateNotifier({
  *
  * @return {object} Returns the object representation of the `patternlab-config.json`
  */
-const getDefaultConfig = function () {
+const getDefaultConfig = function() {
   return defaultConfig;
 };
 
@@ -47,11 +47,11 @@ const getDefaultConfig = function () {
  *
  * @returns {string} current patternlab-node version as defined in `package.json`, as string
  */
-const getVersion = function () {
+const getVersion = function() {
   return packageInfo.version;
 };
 
-const patternlab_module = function (config) {
+const patternlab_module = function(config) {
   const PatternLabClass = require('./lib/patternlab');
   const patternlab = new PatternLabClass(config);
   server = server(patternlab);
@@ -62,7 +62,7 @@ const patternlab_module = function (config) {
      *
      * @returns {string} current patternlab-node version as defined in `package.json`, as string
      */
-    version: function () {
+    version: function() {
       return patternlab.getVersion();
     },
 
@@ -75,7 +75,7 @@ const patternlab_module = function (config) {
      * @param {bool} options.watch whether or not Pattern Lab should watch configured `source/` directories for changes to rebuild
      * @returns {Promise} a promise fulfilled when build is complete
      */
-    build: function (options) {
+    build: function(options) {
       // process.on('unhandledRejection', (reason, p) => {
       //   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
       //   // application specific logging, throwing an error, or other logic here
@@ -135,7 +135,7 @@ const patternlab_module = function (config) {
      *
      * @return {object} Returns the object representation of the `patternlab-config.json`
      */
-    getDefaultConfig: function () {
+    getDefaultConfig: function() {
       return getDefaultConfig();
     },
 
@@ -144,7 +144,7 @@ const patternlab_module = function (config) {
      *
      * @returns {Array<string>} all supported file extensions
      */
-    getSupportedTemplateExtensions: function () {
+    getSupportedTemplateExtensions: function() {
       return patternlab.getSupportedTemplateExtensions();
     },
 
@@ -153,7 +153,7 @@ const patternlab_module = function (config) {
      *
      * @returns {void} Pattern Lab API usage, as console output
      */
-    help: function () {
+    help: function() {
       logger.info(help(patternlab.package.version));
     },
 
@@ -163,7 +163,7 @@ const patternlab_module = function (config) {
      * @param {string} pluginName name of plugin
      * @returns {void}
      */
-    installplugin: function (pluginName) {
+    installplugin: function(pluginName) {
       patternlab.installPlugin(pluginName);
     },
 
@@ -172,7 +172,7 @@ const patternlab_module = function (config) {
      *
      * @returns {Promise} Returns an Array<{name,url}> for the starterkit repos
      */
-    liststarterkits: function () {
+    liststarterkits: function() {
       return patternlab.listStarterkits();
     },
 
@@ -183,7 +183,7 @@ const patternlab_module = function (config) {
      * @param {boolean} clean whether or not to delete contents of source/ before load
      * @returns {void}
      */
-    loadstarterkit: function (starterkitName, clean) {
+    loadstarterkit: function(starterkitName, clean) {
       patternlab.loadStarterKit(starterkitName, clean);
     },
 
@@ -195,7 +195,7 @@ const patternlab_module = function (config) {
      * @param {object} options.data additional data to be merged with global data prior to build
      * @returns {Promise} a promise fulfilled when build is complete
      */
-    patternsonly: function (options) {
+    patternsonly: function(options) {
       if (patternlab && patternlab.isBusy) {
         logger.info(
           'Pattern Lab is busy building a previous run - returning early.'
@@ -221,8 +221,8 @@ const patternlab_module = function (config) {
      */
     server: {
       serve: options => {
-        options.watch = true;
-        return _module.build(options).then(() => server.serve());
+        const _options = Object.assign({}, options, { watch: true });
+        return _module.build(_options).then(() => server.serve());
         //return Promise.resolve();
       },
       reload: server.reload,
--- packages/core/src/lib/server.js
+++ packages/core/src/lib/server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const _ = require('lodash');
 const path = require('path');
 const liveServer = require('@pattern-lab/live-server');
 
